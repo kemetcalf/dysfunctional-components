ec1dd7d4a95f873361733f6957c6b0f11812d1f4
Kat <kemetcalf3.14@gmail.com>
2023-02-09 18:59:02 -0800

    first day, first component

---
+++ dysfunctional-components-app/src/App.js
@@ -0,0 +1,13 @@
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import BasicButton from "./Components.js/no1Button.js";
+
+function App() {
+	return (
+		<div className="App">
+			<BasicButton />
+		</div>
+	);
+}
+
+export default App;
---
+++ dysfunctional-components-app/src/Components.js/no1Button.js
@@ -0,0 +1,15 @@
+import Button from "react-bootstrap/esm/Button";
+
+function BasicButton() {
+	return (
+		<div className="BasicButton">
+			<div className="mb-2">
+				<Button variant="primary" size="lg">
+					Big Button
+				</Button>
+			</div>
+		</div>
+	);
+}
+
+export default BasicButton;
